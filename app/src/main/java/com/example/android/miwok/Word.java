9bc5887646ac869e3fcbcc215510f177b5cb2f53
Sergey Li <pincats@gmail.com>
2020-01-26 20:10:50 +0300

    feat: Add audio files

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mAudioResId;
     private final int NO_VALID_IMAGE = -1;
     private int mImageResourceId = NO_VALID_IMAGE;
 
@@ -18,6 +19,13 @@ public class Word {
         mImageResourceId = imageResourceId;
     }
 
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId, int audioResId) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResId = audioResId;
+    }
+
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
@@ -28,6 +36,10 @@ public class Word {
 
     public int getImageResorceId() {
         return mImageResourceId;
+    }
+
+    public int getAudio() {
+        return mAudioResId;
     }
 
     public boolean hasImage() {
